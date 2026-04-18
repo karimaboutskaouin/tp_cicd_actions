24a0ddf50ac905ca459dac17e1c8838cb14a82f2
HP@DESKTOP-2IFQTQF <karima.boutskaouin@gmail.com>
2026-04-18 15:13:01 +0100

    add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-// eslint.config.mjs
 export default [
-{
-files: ['src/**/*.js'],
-rules: {
-'no-unused-vars': 'warn',
-'no-console': 'warn',
-'eqeqeq': 'error',
-'no-var': 'error',
-'prefer-const': 'warn',
-},
-},
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'eqeqeq': 'error',
+      'no-var': 'error',
+      'prefer-const': 'warn',
+    },
+  },
 ];
